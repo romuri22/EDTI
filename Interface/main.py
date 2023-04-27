f1a28255859ddad8035b862647d078820626131b
AlexMtzLn <a01066904@tec.mx>
2023-04-26 18:41:59 -0600

    blue

--- Interface/main.py
+++ Interface/main.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from app import App
 
 ctk.set_appearance_mode("Dark")
-ctk.set_default_color_theme("/Users/rodrigomurillo/Documents/Python/Generac/generac_orange.json")
+ctk.set_default_color_theme("blue")
 
 
 if __name__ == "__main__":
