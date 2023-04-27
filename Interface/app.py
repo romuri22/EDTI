12c7a58916d95ab8b07b1f43df7d12c94df8d3a7
romuri22 <romurimex@gmail.com>
2023-04-26 18:54:16 -0600

    label1

--- Interface/app.py
+++ Interface/app.py
@@ -7,3 +7,9 @@ class App(ctk.CTk):
         # configure window
         self.title("Engine Signal Simulation Interface")
         self.geometry(f"{1100}x{580}")
+
+
+
+        self.marca = ctk.CTkLabel(self, text="Engine Range")
+        self.marca.place(relx=0.02, rely=0.02)
+
